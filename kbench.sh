7f8ca5d71885b9c217a293fd4ad819d9955ba0c2
Alexander Gladysh <agladysh@gmail.com>
2011-08-26 19:34:30 +0400

    kbench.sh: running only existing interpreters

--- kbench.sh
+++ kbench.sh
@@ -18,9 +18,25 @@ fi
 
 TIME="/usr/bin/time -p"
 
-KBENCH_INTERPRETERS=( 'lua' 'luajit -O' 'luajit2' )
+KBENCH_INTERPRETERS=()
+
+if command -v lua; then
+  KBENCH_INTERPRETERS[${#KBENCH_INTERPRETERS[@]}]="lua"
+fi
+
+if command -v luajit2; then
+  KBENCH_INTERPRETERS[${#KBENCH_INTERPRETERS[@]}]="luajit2"
+fi
+
+if command -v luajit; then
+  # Adding -O option for compatibility with LuaJIT 1.x
+  KBENCH_INTERPRETERS[${#KBENCH_INTERPRETERS[@]}]="luajit -O"
+fi
+
 KBENCH_METHODS=( $(${KBENCH_INTERPRETERS[0]} $KBENCH_SCRIPT | grep -e '^\* \(.*\)$' | awk '{ print $2 }') )
 for interpreter in "${KBENCH_INTERPRETERS[@]}"; do
+  echo "XXX $interpreter"
+  echo "YYY ${KBENCH_METHODS[@]}"
   for method in "${KBENCH_METHODS[@]}"; do
     echo interpreter "'"$interpreter"'" command "'"$KBENCH_SCRIPT"'" method "'"$method"'" num_iter "'"$KBENCH_NUM_ITER"'"
     ${TIME} $interpreter $KBENCH_SCRIPT $method $KBENCH_NUM_ITER
