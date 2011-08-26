2d1f16d111354dcc5d30c067e8336f5406663820
Alexander Gladysh <agladysh@gmail.com>
2011-08-26 20:08:14 +0400

    kbench.sh: KBENCH_INTERPRETERS are overridable now; minor style tuning

--- kbench.sh
+++ kbench.sh
@@ -1,37 +1,39 @@
 #! /bin/bash
 
-if [[ -z "$KBENCH_SCRIPT" ]]; then
+if [[ -z "${KBENCH_SCRIPT}" ]]; then
   KBENCH_SCRIPT=$1;
 fi
 
-if [[ -z "$KBENCH_NUM_ITER" ]]; then
+if [[ -z "${KBENCH_NUM_ITER}" ]]; then
   KBENCH_NUM_ITER=$2;
 fi
 
-if [[ -z "$KBENCH_SCRIPT" ]]; then
+if [[ -z "${KBENCH_SCRIPT}" ]]; then
   KBENCH_SCRIPT=kbench.lua;
 fi
 
-if [[ -z "$KBENCH_NUM_ITER" ]]; then
+if [[ -z "${KBENCH_NUM_ITER}" ]]; then
   KBENCH_NUM_ITER=10000000 # 10M
 fi
 
+if [[ -z "${KBENCH_INTERPRETERS[@]}" ]]; then
+  KBENCH_INTERPRETERS=()
+
+  if command -v lua; then
+    KBENCH_INTERPRETERS[${#KBENCH_INTERPRETERS[@]}]="lua"
+  fi
+
+  if command -v luajit2; then
+    KBENCH_INTERPRETERS[${#KBENCH_INTERPRETERS[@]}]="luajit2"
+  fi
+
+  if command -v luajit; then
+    # Adding -O option for compatibility with LuaJIT 1.x
+    KBENCH_INTERPRETERS[${#KBENCH_INTERPRETERS[@]}]="luajit -O"
+  fi
+fi
+
 TIME="/usr/bin/time -p"
-
-KBENCH_INTERPRETERS=()
-
-if command -v lua; then
-  KBENCH_INTERPRETERS[${#KBENCH_INTERPRETERS[@]}]="lua"
-fi
-
-if command -v luajit2; then
-  KBENCH_INTERPRETERS[${#KBENCH_INTERPRETERS[@]}]="luajit2"
-fi
-
-if command -v luajit; then
-  # Adding -O option for compatibility with LuaJIT 1.x
-  KBENCH_INTERPRETERS[${#KBENCH_INTERPRETERS[@]}]="luajit -O"
-fi
 
 KBENCH_METHODS=( $(${KBENCH_INTERPRETERS[0]} $KBENCH_SCRIPT | grep -e '^\* \(.*\)$' | awk '{ print $2 }') )
 for interpreter in "${KBENCH_INTERPRETERS[@]}"; do
